6a9498b85efe85cced8e6f5651c29648e549e838
Kevin Ren <kevinren1108@gmail.com>
2022-01-17 15:24:38 -0600

    update

--- src/pages/home/companyIntro/index.js
+++ src/pages/home/companyIntro/index.js
@@ -11,13 +11,8 @@ class CompanyIntro extends Component {
           <BgTextBusinessName>Rico <SmallFont>Waterproffing  Material  Canada Co Ltd</SmallFont></BgTextBusinessName>
           <BgTextBusinessIntro>DOHO是在有色金属材料领域有着超过70年的经验与业绩的老牌商社，我们也正在不断努力为制造业领域带来更多创新。 DOHO是在有色金属材料领域有着超过70年的经验与业绩的老牌商社，我们也正在不断努力为制造业领域带来更多创新。 DOHO是在有色金属材料领域有着超过70年的经验与业绩的老牌商社，我们也正在不断努力为制造业领域带来更多创新。 DOHO是在有色金属材料领域有着超过70年的经验与业绩的老牌商社，我们也正在不断努力为制造业领域带来更多创新。 虽然我们经手的材料都是硬材，但我们有着</BgTextBusinessIntro>
         </BgTextWrapper>
-        <BgImage /> 
-        
+        <BgImage />    
       </div>
-      
-        
-   
-
      );
   }
 }
