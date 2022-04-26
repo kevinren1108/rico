70631bd7daa4fb0c9c0aa9cc2c5873a50c0c2ace
kevinren1108 <kevinren1108@gmail.com>
2022-04-26 09:58:36 -0600

    update

--- src/pages/home/companyIntro/index.js
+++ src/pages/home/companyIntro/index.js
@@ -10,7 +10,7 @@ class CompanyIntro extends Component {
           <BgTextBusinessName>Rico <SmallFont>Waterproffing  Material  Canada Co Ltd</SmallFont></BgTextBusinessName>
           <BgTextBusinessIntro>
             Founded in 2020, Rico Waterproofing Materials Canada Co.Ltd.  sets out itself to be one of the leading construction material suppliers in Western Canada. 
-            The company specializes in supplying variety of waterproofing adhesives and paints that suite the climate in Canada. At Rico Waterproofing Materials Canada Co.Ltd., We strive to provide our customers with high quaily, affordble waterproofing and building solutions. We value the long term realtion with our customers and archieve complete customer satisifaction is our ultimate goal. 
+            The company specializes in supplying variety of waterproofing adhesives and paints that suite the climate in Canada. At Rico Waterproofing Materials Canada Co.Ltd., We strive to provide our customers with high quality, affordable waterproofing and building solutions. We value the long term realtion with our customers and archieve complete customer satisifaction is our ultimate goal. 
             Choose Rico, Choose peace of mind!
           </BgTextBusinessIntro>
         </BgTextWrapper>
